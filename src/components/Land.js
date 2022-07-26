c22a02e148da3929f42f2d5e417275d078ad48c1
Mwenje <mwenje24@gmail.com>
2022-07-26 13:53:27 +0300

    designed the land spaces page

--- src/components/Land.js
+++ src/components/Land.js
@@ -1,10 +1,61 @@
 import React from 'react'
+import image from "../images/land2.jpeg"
 
 function Land() {
   return (
-    <div>
-      land
-    </div>
+    <>
+      <span className='page-headings'>land Spaces</span>
+        <div className='popular-property row'>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+          <div className='card popular-card col-4'>
+            <img src={image} className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">Card title <span>Charges<button className='btn btn-sm btn-outline-secondary disabled'>status</button></span></h5>
+              <h6 className="card-text">............Address</h6>
+              <h6 className="card-text">............Measurements <span>Bids</span></h6>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
 
